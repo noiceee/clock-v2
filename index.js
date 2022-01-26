741a429e695682764db00546f63319f66e283a37
Kartikey Singh <codingwalaid@gmail.com>
2022-01-27 01:20:46 +0530

    Stores preferences in local storage, fixed bugs, added Social media profiles

--- index.js
+++ index.js
@@ -2,12 +2,44 @@ const minuteHand = document.querySelector(".minute-hand");
 const secondHand = document.querySelector(".second-hand");
 const hourHand = document.querySelector(".hour-hand");
 const timeDiv = document.querySelector(".time-container");
-let currentTheme = "dark";
-let isRGB = "off";
-let isRed = "off";
+
+let currentTheme =  window.localStorage.getItem("theme") || "light";
+document.documentElement.setAttribute("color-theme", currentTheme);
+if(currentTheme == "light"){
+    document.querySelector('.theme-toggle').classList.add('off');
+}
+
+let isRGB = window.localStorage.getItem("rgb") || "green-color";
+document.documentElement.setAttribute("rgb", isRGB);
+if(isRGB == "off"){
+    document.querySelector('.theme-toggle.rgb').classList.remove("off");
+    document.querySelector(".menu-wrapper:nth-child(3)").setAttribute("style", "display:none;");
+}else{
+    let animationSpeed = window.localStorage.getItem("animation-speed") || 16;
+    document.querySelector(".inner-border").setAttribute("style", `animation: rgb-animation ${animationSpeed}s linear infinite`);
+}
+document.querySelectorAll(".color-box").forEach((colorBox)=>{
+    if(colorBox.classList.contains(isRGB)){
+        colorBox.classList.add("active");
+    }else{
+        colorBox.classList.remove("active");
+    }
+})
+
+let isRed = window.localStorage.getItem("red") || "off";
+document.documentElement.setAttribute("red", isRed);
+if(isRed == "on"){
+    document.querySelector('.theme-toggle.red').classList.add("off");
+}
+
+let showLabel = window.localStorage.getItem("showLabel") || "false";
+document.documentElement.setAttribute("show-label", showLabel);
+if(showLabel == "true"){
+    document.querySelector('.theme-toggle.rgb').classList.remove('off');
+}
 
 setClock();
-document.documentElement.setAttribute("rgb", "off");
+
 setInterval(setClock, 1000);
 
 function setClock(){
@@ -30,22 +62,25 @@ function setClock(){
       })}`;
 }
 
-document.querySelector('.theme-toggle').addEventListener('click', ()=>{
-    if(currentTheme!="light"){
-        document.documentElement.setAttribute("color-theme", "light");
-        currentTheme = "light";
-        document.querySelector('.theme-toggle').classList.add('off');
-    }
-    else{
+function setTheme(){
+    if(currentTheme!="dark"){
         document.documentElement.setAttribute("color-theme", null);
         document.querySelector('.theme-toggle').classList.remove('off');
         currentTheme = "dark";
+        window.localStorage.setItem("theme", "dark");
     }
-})
-document.querySelector('.theme-toggle.rgb').addEventListener('click', ()=>{
+    else{
+        document.documentElement.setAttribute("color-theme", "light");
+        currentTheme = "light";
+        document.querySelector('.theme-toggle').classList.add('off');
+        window.localStorage.setItem("theme", "light");
+    }
+}
+function setRGB(){
     if(isRGB=="off"){
         document.documentElement.setAttribute("rgb", "all-color");
-        isRGB = "on";
+        isRGB = "all-color";
+        window.localStorage.setItem("rgb", isRGB);
         document.querySelector('.theme-toggle.rgb').classList.add("off");
         document.querySelector(".menu-wrapper:nth-child(3)").setAttribute("style", "display:block;");
     }
@@ -53,35 +88,73 @@ document.querySelector('.theme-toggle.rgb').addEventListener('click', ()=>{
         document.documentElement.setAttribute("rgb", "off");
         document.querySelector('.theme-toggle.rgb').classList.remove('off');
         isRGB = "off";
+        window.localStorage.setItem("rgb", isRGB);
         document.querySelector(".menu-wrapper:nth-child(3)").setAttribute("style", "display:none;");
     }
-})
-document.querySelector('.theme-toggle.red').addEventListener('click', ()=>{
+}
+function setRED(){
     if(isRed=="off"){
         document.documentElement.setAttribute("red", "on");
         isRed = "on";
+        window.localStorage.setItem("red", isRed);
+        document.documentElement.setAttribute("red", isRed);
         document.querySelector('.theme-toggle.red').classList.add("off");
     }
     else{
         document.documentElement.setAttribute("red", "off");
         document.querySelector('.theme-toggle.red').classList.remove('off');
         isRed = "off";
+        window.localStorage.setItem("red", isRed);
+        document.documentElement.setAttribute("red", isRed);
     }
-})
+}
+function setLabel(){
+    if(showLabel=="false"){
+        document.documentElement.setAttribute("show-label", "true");
+        showLabel = "true";
+        window.localStorage.setItem("showLabel", showLabel);
+        document.querySelector('.theme-toggle.label').classList.add("off");
+    }
+    else{
+        document.documentElement.setAttribute("show-label", "false");
+        document.querySelector('.theme-toggle.label').classList.remove('off');
+        showLabel = "false";
+        window.localStorage.setItem("showLabel", showLabel);
+    }
+}
+
+document.querySelector('.theme-toggle').addEventListener('click', setTheme);
+document.querySelector('.theme-toggle.rgb').addEventListener('click', setRGB);
+document.querySelector('.theme-toggle.red').addEventListener('click', setRED);
+document.querySelector('.theme-toggle.label').addEventListener('click', setLabel);
 
 const colorOptions = ["all-color", "green-color", "purple-color", "pink-color"];
 for(i=0; i<4; i++){
     let currentColor = colorOptions[i];
     document.querySelector(`.color-box.${currentColor}`).addEventListener("click", ()=>{
         document.documentElement.setAttribute("rgb", currentColor);
+        window.localStorage.setItem("rgb", currentColor);
         document.querySelectorAll(".color-box").forEach((colorBox)=>{
-            console.log(colorBox.classList.contains(currentColor));
             if(colorBox.classList.contains(currentColor)){
                 colorBox.classList.add("active");
-                console.log(colorBox);
             }else{
                 colorBox.classList.remove("active");
             }
         })
     })
 }
+
+document.querySelector(".speed-slider input").addEventListener('input', (e)=>{
+    document.querySelector(".inner-border").setAttribute("style", `animation: rgb-animation ${e.currentTarget.value/20}s linear infinite`);
+    window.localStorage.setItem("animation-speed", e.currentTarget.value/20);
+})
+
+document.querySelector(".github").addEventListener('click', ()=>{
+    window.location.href = "http://github.com/noiceee";
+})
+document.querySelector(".linkedin").addEventListener('click', ()=>{
+    window.location.href = "https://in.linkedin.com/in/kartikey69";
+})
+document.querySelector(".codechef").addEventListener('click', ()=>{
+    window.location.href = "https://www.codechef.com/users/noicee";
+})
